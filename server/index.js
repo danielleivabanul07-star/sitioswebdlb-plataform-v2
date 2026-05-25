a65ad5a04f48ee9e6b08fce2f3d539ceed9d6ddd
Daniel Leyva Banul <danielleivabanul07@gmail.com>
2026-05-25 12:27:56 -0400

    Increase backend save limit

--- server/index.js
+++ server/index.js
@@ -17,7 +17,19 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
+
+app.use(
+  express.json({
+    limit: "50mb"
+  })
+);
+
+app.use(
+  express.urlencoded({
+    limit: "50mb",
+    extended: true
+  })
+);
 
 // =========================
 // TEST SERVER
